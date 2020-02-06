f0d3445e86d7cda536712694fefaffe94990ac45
Jason Farrell <jason@useallfive.com>
2020-02-06 14:21:25 -0800

    Prod updates.

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.get('/api/metadata', async (req, res) => {
 })
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
